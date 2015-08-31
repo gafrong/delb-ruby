783eb07e6cb1d3c92a88b7dc82a1ef094ebff4a4
Ji Lee <gafrong@gmail.com>
2015-08-31 13:40:11 +1000

    sign up working

--- ng-app/app/scripts/controllers/user_sessions.js
+++ ng-app/app/scripts/controllers/user_sessions.js
@@ -10,6 +10,7 @@
 angular.module('App')
   .controller('UserSessionsCtrl', ['$scope', function ($scope) {
     $scope.$on('auth:login-error', function(ev, reason) {
-      $scope.error = reason.errors[0];
+      // $scope.error = reason.errors[0];
+      $scope.error = "something wrong";
     });
   }]);

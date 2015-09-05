4530e7382a9858455af2256cf667e9f5c4b9e2ef
Ji Lee <gafrong@gmail.com>
2015-09-05 19:23:54 +1000

    client side login working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ActiveRecord::Base
     self.uid = SecureRandom.uuid
     skip_confirmation!
   end
+
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
 end

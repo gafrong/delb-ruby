48e2ea678a1b62c648727ff862b8cca11e0a455c
Ji Lee <gafrong@gmail.com>
2015-11-18 09:13:41 +1100

    removed renee model

--- app/api/product/data.rb
+++ app/api/product/data.rb
@@ -11,35 +11,5 @@ module Product
         asset.inspect
       end
     end
-
-    resource :upload do
-      post do
-          # takes the :avatar value and assigns it to a variable
-          avatar = params[:avatar]
-
-          # the avatar parameter needs to be converted to a
-          # hash that paperclip understands as:
-          attachment = {
-              :filename => avatar[:filename],
-              :type => avatar[:type]
-          }
-
-          # creates a new User object
-          userx = Userx.new
-
-          # This is the kind of File object Grape understands so let's
-          # pass the hash to it
-          userx.avatar = ActionDispatch::Http::UploadedFile.new(attachment)
-
-          # easy
-          userx.avatar_path = attachment[:filename]
-
-          # even easier
-          userx.name = "dummy name"
-
-          # and...
-          userx.save
-      end
-    end
   end
 end

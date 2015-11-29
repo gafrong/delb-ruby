16aa4db2c3bee11d0f82600a359ffb74fca3886f
Ji Lee <gafrong@gmail.com>
2015-11-29 17:50:47 +1100

    gif image

--- ng-app/app/scripts/controllers/item.js
+++ ng-app/app/scripts/controllers/item.js
@@ -9,6 +9,7 @@ angular.module('App')
   .controller('ItemDetailCtrl', ['$scope', '$routeParams', 'Item',
     function ($scope, $routeParams, Item){
       console.log($routeParams.id);
+      $scope.loaderSource = 'images/default.gif';
       $scope.loading = true;
       Item.getItem({id: $routeParams.id})
       .success(function(returnData){

039dffa254d01863eb3a475ed93e2abea40fbcc9
Ji Lee <gafrong@gmail.com>
2015-12-03 14:40:08 +1100

    figaro carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'xxx',
-    :aws_secret_access_key  => 'yyy'
+    :aws_access_key_id      => ENV["aws_access_key_id"],
+    :aws_secret_access_key  => ENV["aws_secret_access_key"]
   }
-  config.fog_directory = 'name_of_directory'
+  config.fog_directory = ENV["fog_directory"]
 end

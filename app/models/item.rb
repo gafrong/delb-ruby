03f55dc0ff9518b1bf74cabea9addaa6ebbbe9ec
Ji Lee <gafrong@gmail.com>
2015-12-04 09:58:49 +1100

    aws

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   # mount_uploader :image, ImageUploader
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"},
+  has_attached_file :image,
     storage: :s3,
     s3_credentials: {
       access_key_id: ENV["AWS_ACCESS_KEY_ID"], 
-      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-      },
-    bucket: "delbfiles"
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      bucket: "delbfiles"
+      }
 # , :path => ":rails_root/public/images/uploads/:filename", default_url => "delb.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

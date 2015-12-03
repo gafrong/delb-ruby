844455be4b1740d9a898f9228d2ab39810040096
Ji Lee <gafrong@gmail.com>
2015-12-03 17:29:32 +1100

    s3 upload w/ paperclip

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   # mount_uploader :image, ImageUploader
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"}, :path => ":rails_root/public/images/uploads/:filename", default_url => "delb.png"
-
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"},
+    storage: :s3,
+    s3_credentials: {access_key_id: ENV["AWS_KEY"], secret_access_key: ENV["AWS_SECRET"]},
+    bucket: "YOUR_BUCKET"
+# , :path => ":rails_root/public/images/uploads/:filename", default_url => "delb.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

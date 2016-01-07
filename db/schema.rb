639197bbcb17302eb2b6bea513b9ed986c2b7191
Ji Lee <gafrong@gmail.com>
2016-01-08 07:59:55 +1100

    user id to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201064502) do
+ActiveRecord::Schema.define(version: 20160107204645) do
 
   create_table "assets", force: :cascade do |t|
     t.string   "title"
@@ -59,13 +59,16 @@ ActiveRecord::Schema.define(version: 20151201064502) do
     t.text     "images"
     t.datetime "created_at",                                  null: false
     t.datetime "updated_at",                                  null: false
-    t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "image_path"
+    t.string   "location"
+    t.integer  "user_id"
   end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
